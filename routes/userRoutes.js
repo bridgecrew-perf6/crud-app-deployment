8b715a9d2e0380242dcc106e87c827c89cae3713
SAYAN MAITI <sayan.maiti@codelogicx.com>
2022-03-22 18:49:59 +0530

    update

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -13,7 +13,7 @@ module.exports = {
           description: 'Home Page',
           tags: ['api'],
           handler: () => {
-            return "Hello Hupi! Welcome to Sqan's Application!"
+            return "Hello Hupi! Welcome to Saan's Application!"
           },
         },
       },
