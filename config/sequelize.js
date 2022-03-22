57f7644a61c95986c4c4af94dc2e4bf530638154
SAYAN MAITI <sayan.maiti@codelogicx.com>
2022-03-22 18:01:48 +0530

    update

--- config/sequelize.js
+++ config/sequelize.js
@@ -3,7 +3,7 @@ const Sequelize = require("sequelize");
 const { DB_DIALECT, DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT, DB_USERNAME } = require('./env');
 
 const sequelize = new Sequelize(DB_NAME, DB_USERNAME, DB_PASSWORD, {
-    dialect: DB_DIALECT,
+    dialect: "mysql",
     host: DB_HOST,
     port: DB_PORT,
     logging: false,
